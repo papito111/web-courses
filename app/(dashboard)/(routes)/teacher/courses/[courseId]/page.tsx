3bddc2b0e1d787ffd0f24afa3125e800655bcae6
papito111 <adrian.kurzac@wp.pl>
2025-01-26 12:18:02 +0100

    icon conf

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -3,6 +3,9 @@ import { db } from '@/lib/db'
 import { auth } from "@clerk/nextjs/server";
 import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
+import { IconBadge } from '@/components/icon-badge';
+import { LayoutDashboard } from 'lucide-react';
+
 
 const CourseIdPage = async ({ params }:
     {
@@ -42,17 +45,18 @@ const CourseIdPage = async ({ params }:
             <div className='flex items-center justify-between'>
                 <div className='flex flex-col gap-y-2'>
                     <h1 className='text-2xl font-semibold'>
-                        Course setup
+                        Course setup: {course.title}
                     </h1>
                     <span className='text-sm text-slate-700'>
                         Complete all fields({completionText})
                     </span>
                 </div>
             </div>
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-6'>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-3 mt-6'>
                  {/* //tu moge zmienic bo teraz mi troche ucieka na lewoo */}
                 <div>
                     <div className='flex items-center gap-x-2'>
+                        <IconBadge icon={LayoutDashboard} size="sm" variant="success"/>
                         <h2 className='text-xl'>
                             Customize your course
                         </h2>
