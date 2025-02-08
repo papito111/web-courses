fad9a0354a96268e6ab347be3446ef9b15371abd
papito111 <adrian.kurzac@wp.pl>
2025-02-08 09:22:57 +0100

    chapter  list changes

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
@@ -1,6 +1,16 @@
 'use client'
 
 import React from 'react'
+import {Chapter} from '@prisma/client'
+import { UpdateData } from 'firebase/firestore';
+
+
+interface ChaptersList {
+  items: Chapter[];
+  onReorder: (updateData:{id:string;position: number}[]) => void;
+  
+}
+
 
 export const ChaptersList = () => {
   return (
