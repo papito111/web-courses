b769bc9a1ce84886100584be4519450cc626e497
papito111 <adrian.kurzac@wp.pl>
2025-02-15 17:02:46 +0100

    publishing

---
+++ app/api/courses/[courseId]/chapters/[chapterId]/publish/route.tsx
@@ -0,0 +1,56 @@
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import { db } from "@/lib/db";
+import { NextRequest } from "next/server";
+
+
+
+export async function PATCH(req:NextRequest,{params}:{params:{courseId:string, chapterId:string}}) {
+
+    try {
+        const {userId} = getAuth(req);
+
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        const ownCourse = await db.course.findUnique({
+            where: {
+                id: params.courseId,
+                userId
+            },
+        });
+
+        if(!ownCourse){
+            return new NextResponse("Unathoarrized",{status:401})
+        }
+
+        const chapter = await db.chapter.findUnique({
+            where:{
+                id:params.chapterId,
+                courseId: params.courseId
+            }
+        });
+        if(!chapter){
+            return new NextResponse("there is no such a chapter",{status:401})
+        }
+
+        const muxData = await db.muxData.findUnique({
+            where:{
+                chapterId:params.chapterId
+            }
+        });
+
+        if (!chapter|| !muxData || !chapter.title || !chapter.description || !chapter.videoUrl){
+        return new NextResponse("Missing data",{status:400})
+
+        }
+    }catch(error){
+        console.log("published incorecctly",error)
+        return new NextResponse("Internal error",{status:500})
+        
+
+    }finally{
+
+    }
+}
