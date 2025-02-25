b106316da8301a93f5782eef4588f90969ad58c0
papito111 <adrian.kurzac@wp.pl>
2025-02-25 16:55:13 +0100

    course publishing cd

--- app/api/courses/[courseId]/publish/route.ts
+++ app/api/courses/[courseId]/publish/route.ts
@@ -10,6 +10,28 @@ import {Mux} from "@mux/mux-node"
 export async function PATCH(req:NextRequest,{params}:{params:{courseId:string, chapterId:string}}){
     try{
         const {userId} = getAuth(req);
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+        const course = await db.course.findUnique({
+            where:{
+                id: params.courseId,
+                userId
+            },
+            include: {
+                chapters:{
+                    include:{
+                        muxData: true
+                    }
+                }
+            }
+        })
+        if (!course) {
+            return new NextResponse("No course to be published", { status: 401 });
+        }
+
+        const hasPublishedChapter = course.chapters.some((chapter) => chapter.isPublished);
+        
 
     }catch(error){
         return new NextResponse("some error in chapter title",{status:500})
