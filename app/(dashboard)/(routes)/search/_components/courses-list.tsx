e5d1db212f3b1b7c7e6c0d9cec858244fdb0e122
papito111 <adrian.kurzac@wp.pl>
2025-03-01 12:18:50 +0100

    without categoreis info

--- app/(dashboard)/(routes)/search/_components/courses-list.tsx
+++ app/(dashboard)/(routes)/search/_components/courses-list.tsx
@@ -15,24 +15,25 @@ export const CourseList = ({items}: CourseListProps) => {
 
     return(
         <div>
-            <div className="grid sm-grid-cold-2  md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+            <div className="grid sm-grid-cold-2  md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3 gap-4">
         {items.map((item)=>(
             <CourseCard
             key = {item.id}
             id = {item.id}
             title = {item.title}
-            imageUrl = {item.imageUrl}
+            imageUrl = {item.imageUrl!}
             chaptersLength = {item.chapters.length}
-            price={item.price}
+            price={item.price!}
             progress={item.progress}
-            category={item?.category?.name} 
+            category={item?.category?.name!} 
             />
         
         ))}
         </div>
         {items.length === 0 &&(
-            <div>
-                No courses
+            <div className="text-center text-gray-500 mt-4">
+                <p className="text-lg">Brak dostępnych kursów</p>
+                <p className="text-sm">Wróć tu za jakiś czas, lub sprawdź inne kategorie.</p>
             </div>
         )}
         </div>
--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -17,21 +17,11 @@ interface SearchPageProps {
 
 const SearchPage = async ( { searchParams }: SearchPageProps) => {
 
-    // const res = await fetch(`/api/user`, { cache: "no-store" });
     const { userId } = await auth();
-    // if (res.status === 401) {
-    //     return <p className="text-red-500">Unauthorized</p>;
-    // }
-
-    // if (!res.ok) {
-    //     return <p className="text-red-500">Error fetching user data</p>;
-    // }
-
-    // const { userId } = await res.json();
-
+   
     if(!userId){
         return redirect("/")
-        // return new NextResponse("unathoarize",{status:401})
+
     }
     const categories = await db.category.findMany({
         orderBy: {
@@ -39,19 +29,20 @@ const SearchPage = async ( { searchParams }: SearchPageProps) => {
         }
     });
     const courses = await GetCourses({
-        userId, 
-        ...searchParams
-    })
+        userId,
+        title: searchParams.title,
+        categoryId: searchParams.categoryId || undefined
+    });
     return (
-        <div className="p-5 w-full mx-auto">
-            <div className="flex items-center mb-4">
+        <div className="p-5 w-full mx-auto ">
+            <div className="flex items-center mb-6">
                 <SearchInput />
             </div>
-            <div>
+            <div className="mb-6">
                 <Categories items={categories} />
             </div>
             {/* <GetCourses /> */}
-            <div>
+            <div >
                 <CourseList items={courses}/>
             </div>
         </div>
