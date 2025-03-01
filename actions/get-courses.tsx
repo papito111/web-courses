36f7fe991b191278e402ec141675f8f7268cba5e
papito111 <adrian.kurzac@wp.pl>
2025-03-01 10:28:59 +0100

    get courses tsx

--- actions/get-courses.tsx
+++ actions/get-courses.tsx
@@ -1,7 +1,8 @@
 import { Category, Course } from "@prisma/client";
 
 import { getProgress } from "./get-progress";
-import { db } from "@/app/firebaseConfig";
+import { db } from "@/lib/db";
+import { Container } from "lucide-react";
 
 type CourseWithProgressWithCategory = Course & {
     category: Category | null;
@@ -11,10 +12,71 @@ type CourseWithProgressWithCategory = Course & {
 
 type GetCourses = {
     userId: string;
-    tit
-}
+    title?: string;
+    categoryId?: string;
+};
 
 
 export const GetCourses = async ({
+    userId,
+    title,
+    categoryId
 
-})
+}:GetCourses): Promise<CourseWithProgressWithCategory[]> => {
+    try{
+        const courses = await db.course.findMany({
+            where: {
+                isPublished: true,
+                title: {
+                    contains: title
+                },
+                categoryId,
+            },
+            include: {
+                category: true,
+                chapters: {
+                    where: {
+                        isPublished: true,
+                    },
+                    select: {
+                        id:true,
+                    }
+                    
+                    
+                },
+                purchases: {
+                    where: {
+                        userId
+                    }
+                }
+            },
+            orderBy: {
+                createdAt: "desc"
+            }
+
+        })
+        const courseWithProgress: CourseWithProgressWithCategory[] = await
+        Promise.all(
+            courses.map(async course=> {
+                if(course.purchases.length === 0) {
+                    return {
+                        ...course,
+                        progress: null,
+                    }
+                }
+                const progressPercentage = await getProgress(userId, course.id);
+                return {
+                    ...course,
+                    progress: progressPercentage,
+                }
+            })
+        ) 
+        
+
+        return courseWithProgress;
+    }
+    catch(error){
+        console.log("getcourses", error)
+        return[]
+    }
+}
