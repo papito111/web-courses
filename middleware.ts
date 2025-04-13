1b0de424d6b5238020401fc04eaabd225c48feb2
papito111 <adrian.kurzac@wp.pl>
2025-04-13 17:59:44 +0200

    musi byc middleware

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,28 @@
-// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-// import { NextResponse } from 'next/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
-// // const isPublicRoute = createRouteMatcher(["/api/webhook"]);
-// const isPublicRoute = createRouteMatcher([
-//   "/api/stripe/webhook",
-// ]);
+// const isPublicRoute = createRouteMatcher(["/api/webhook"]);
+const isPublicRoute = createRouteMatcher([
+  "/api/stripe/webhook",
+]);
 
 
-// export default clerkMiddleware((auth, req) => {
-//   if (!isPublicRoute(req)) {
-//     const authResult = auth.protect();
-//     if (authResult instanceof NextResponse) {
-//       return authResult;
-//     }
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) {
+    const authResult = auth.protect();
+    if (authResult instanceof NextResponse) {
+      return authResult;
+    }
     
-//   }
-//   return NextResponse.next();
-// });
+  }
+  return NextResponse.next();
+});
 
 
-// export const config = {
-//   matcher: [
+export const config = {
+  matcher: [
     
-//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-//     '/(api|trpc)(.*)',
-//   ],
-// };
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
+};
